3190111c408abb8f721244ced71d8d50c5b58743
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-10-23 17:51:28 +0200

    updates somes files

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,11 +1,12 @@
 import {createFakeContact} from "../utils/createFakeContact.js";
-import {readSongs} from "../utils/readContacts.js";
+import {readContacts} from "../utils/readContacts.js";
 import {writeContacts} from "../utils/writeContacts.js";
 
 export const addOneContact = async () => {
-    const contacts = await readSongs();
+    const contacts = await readContacts();
     const newContact = createFakeContact();
     await writeContacts([...contacts, newContact]);
 };
 
 addOneContact();
+
